5ee76fe15bf779504d8fcb3d1dd9217f400db6b4
Jonathan Woodrow <150376162+Jwoodrow15@users.noreply.github.com>
2024-05-10 16:26:42 +0000

    use ggplot and geom_point/geom_bar to create some visualizations from the palmerpenguins.

--- R Visualization Practice.R
+++ R Visualization Practice.R
@@ -1,3 +1,17 @@
+#Install packages/Load libraries
+install.packages("ggplot2")
+install.packages("palmerpenguins")
+library(ggplot2)
+library(palmerpenguins)
+data(penguins)
+View(penguins)
+
+##penguin visualization using ggplot and geom_point
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
+#same visualization but written a different way
 ggplot(data = penguins, mapping = aes(x = flipper_length_mm, y = body_mass_g)) +  geom_point()
-test
-test
+
+#some practice
+ggplot(data = penguins, mapping = aes(x = island)) +  geom_bar(color = "blue")
+
