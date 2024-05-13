2171fd9866bac1e40785024bcfe7ccbab5406188
Jonathan Woodrow <150376162+Jwoodrow15@users.noreply.github.com>
2024-05-13 15:45:45 +0000

    practice with different aesthetic variables - color, shape, size, alpha.

---
+++ ggplot2 - aesthetic variables.R
@@ -0,0 +1,24 @@
+#load initial library
+library(ggplot2)
+library(palmerpenguins)
+
+#initial ggplot to start with
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g))
+
+#add another variable - color and map it to species.
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species))
+
+#instead of color, change it to shapes.
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,shape=species))
+
+#map both color and shape to species.
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,shape=species,color=species))
+
+#also add in size mapped to species
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,shape=species,color=species,size=species))
+
+#map alpha aesthetic to species (makes them more or less transparent)
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,alpha=species))
+
+#change all to purple
+ggplot(data=penguins) + geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g),color="purple")
