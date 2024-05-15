6ceebdfe9a580764e62d3a5760229ad1b7bba014
Jonathan Woodrow <150376162+Jwoodrow15@users.noreply.github.com>
2024-05-15 19:36:38 +0000

    Add save files with built in R functions pdf() and png().

--- export and save.R
+++ export and save.R
@@ -11,3 +11,21 @@ ggplot(data=penguins)+
 
 #ggsave - will save the last plot you made.
 ggsave("Three Penguin Species.png")
+
+#Using graphics devices to print and save. Uses built in R code rather than ggsave.
+
+#Example to use png()
+png(file = "exampleplot.png", bg = "transparent")
+plot(1:10)
+rect(1, 5, 3, 7, col = "white")
+dev.off()
+
+#example using pdf()
+pdf(file = "/Users/jonathanwoodrow/Desktop/example.pdf",    
+    width = 4,     
+    height = 4) 
+plot(x = 1:10,     
+     y = 1:10)
+abline(v = 0)
+text(x = 0, y = 1, labels = "Random text")
+dev.off()
